542f6bacef3cf3a09dcc735abc16634905edaf62
David Ketchum <dgketchum@gmail.com>
2016-08-06 14:38:48 -0400

    added doc string to dict_setup.initialize_master_dict

--- recharge/dict_setup.py
+++ recharge/dict_setup.py
@@ -29,7 +29,8 @@ from recharge.raster_manager import ManageRasters
 
 
 def initialize_master_dict(list_of_variables, raster_shape):
-
+    """create an empty dict that will carry ETRM-drived values day to day"""
+    
     dictionary = {}
     for key in list_of_variables:
         dictionary.update({key: ones(raster_shape)})
