4cfb0478b63b8567dfa80de29e6c0e9c78da8007
Jake Ross <jirhiker@gmail.com>
2017-02-20 11:39:26 -0700

    removed self._ones, self._zeros from Processes

--- recharge/etrm_processes.py
+++ recharge/etrm_processes.py
@@ -80,7 +80,7 @@ class Processes(object):
         self._initial = time_it(initialize_initial_conditions_dict, initial_inputs, self._mask_path)
         self._master = time_it(initialize_master_dict, self._shape)
 
-        self._ones, self._zeros = ones(self._shape), zeros(self._shape)
+        # self._ones, self._zeros = ones(self._shape), zeros(self._shape)
         self._raster_manager = RasterManager(static_inputs, polygons, date_range, output_root,
                                              write_freq)  # self._outputs
 
@@ -119,20 +119,20 @@ class Processes(object):
                 m['soil_ksat'] = s['soil_ksat'] * 6 / 24.
 
             if sensitivity_matrix_column:
-                time_it(self._do_parameter_adjustment, sensitivity_matrix_column)
+                time_it(self._do_parameter_adjustment, m, s, sensitivity_matrix_column)
 
             # self._do_dual_crop_coefficient(tm_yday, m, s, c)
             # self._do_snow()
             # self._do_soil_ksat_adjustment(m, s)
 
             time_it(self._do_dual_crop_coefficient, tm_yday, m, s, c)
-            time_it(self._do_snow)
+            time_it(self._do_snow, m, c)
             time_it(self._do_soil_ksat_adjustment, m, s)
 
             if swb_mode == 'fao':
                 time_it(self._do_fao_soil_water_balance, m, s, apply_rofrac)
             elif swb_mode == 'vertical':
-                time_it(self._do_vert_soil_water_balance, apply_rofrac, allen_ceff)
+                time_it(self._do_vert_soil_water_balance, m, s, apply_rofrac, allen_ceff)
 
             time_it(self._do_mass_balance, day, swb=swb_mode)
 
@@ -142,7 +142,7 @@ class Processes(object):
                 self.tracker = initialize_master_tracker(m)
 
             time_it(rm.update_raster_obj, m, self._mask_path, day)
-            time_it(self._update_master_tracker, day)
+            time_it(self._update_master_tracker, m, day)
 
             m['first_day'] = False
 
@@ -156,11 +156,11 @@ class Processes(object):
         m = self._master
 
         m['first_day'] = True
-        m['albedo'] = self._ones * 0.45
-        m['swe'] = self._zeros  # this should be initialized correctly using simulation results
+        m['albedo'] = ones(self._shape) * 0.45
+        m['swe'] = zeros(self._shape)  # this should be initialized correctly using simulation results
         # s['rew'] = minimum((2 + (s['tew'] / 3.)), 0.8 * s['tew'])  # this has been replaced
         # by method of Ritchie et al (1989), rew derived from percent sand/clay
-        m['dry_days'] = self._ones
+        m['dry_days'] = zeros(self._shape)
 
         m['pdr'], m['dr'] = self._initial['dr'], self._initial['dr']
         m['pde'], m['de'] = self._initial['de'], self._initial['de']
@@ -302,7 +302,7 @@ class Processes(object):
 
         ke = where(ke_init > few * kc_max, few * kc_max, ke_init)
         ke = where(ke_init < zeros(ke.shape), zeros(ke.shape) + 0.01, ke)
-        ke_adjustment = where(ke_init > few * kc_max, ke / ke_init, self._ones)
+        ke_adjustment = where(ke_init > few * kc_max, ke / ke_init, 1)
 
         m['ke'] = minimum(ke, 1)
         m['ke_init'] = ke_init
@@ -314,13 +314,11 @@ class Processes(object):
         m['evap_2'] = e2 = st_2_dur * kr * (kc_max - (ks * kcb)) * etrs * ke_adjustment
         m['evap'] = e1 + e2
 
-    def _do_snow(self):
+    def _do_snow(self, m, c):
         """ Calibrated snow model that runs using PRISM temperature and precipitation.
 
         :return: None
         """
-        m = self._master
-        c = self._constants
 
         temp = m['temp']
         palb = m['albedo']
@@ -330,13 +328,13 @@ class Processes(object):
         a_min = c['a_min']
         a_max = c['a_max']
 
-        sf = where(temp < 0.0, precip, self._zeros)
-        rain = where(temp >= 0.0, precip, self._zeros)
+        sf = where(temp < 0.0, precip, 0)
+        rain = where(temp >= 0.0, precip, 0)
         # alb = where(m['snow_fall'] > 3.0, self._ones * c['a_max'], palb)
         # alb = where(m['snow_fall'] <= 3.0, c['a_min'] + (palb - c['a_min']) * exp(-0.12), alb)
         # alb = where(m['snow_fall'] == 0.0, c['a_min'] + (palb - c['a_min']) * exp(-0.05), alb)
         # alb = where(alb < c['a_min'], c['a_min'], alb)
-        alb = where(sf > 3.0, self._ones * a_max, palb)
+        alb = where(sf > 3.0, a_max, palb)
         alb = where(sf <= 3.0, a_min + (palb - a_min) * exp(-0.12), alb)
         alb = where(sf == 0.0, a_min + (palb - a_min) * exp(-0.05), alb)
         palb = where(alb < a_min, a_min, alb)
@@ -394,7 +392,7 @@ class Processes(object):
         water = m['rain'] + melt
 
         # update number of days of dry weather
-        m['dry_days'] = where(water < 0.1, m['dry_days'] + self._ones, self._ones)
+        m['dry_days'] = where(water < 0.1, m['dry_days'] + 1, 1)
 
         # print 'shapes: rain is {}, melt is {}, water is {}'.format(m['rain'].shape, m['melt'].shape, water.shape)
         # it is difficult to ensure mass balance in the following code: do not touch/change w/o testing #
@@ -432,23 +430,23 @@ class Processes(object):
         #                                                          mm_af(m['transp']))
 
         # water balance through skin layer #
-        drew = where(water >= pdrew + evap_1, self._zeros, pdrew + evap_1 - water)
-        water = where(water < pdrew + evap_1, self._zeros, water - pdrew - evap_1)
+        drew = where(water >= pdrew + evap_1, 0, pdrew + evap_1 - water)
+        water = where(water < pdrew + evap_1, 0, water - pdrew - evap_1)
         # print 'water through skin layer: {}'.format(mm_af(water))
-        m['ro'] = where(water > soil_ksat, water - soil_ksat, self._zeros)
+        m['ro'] = where(water > soil_ksat, water - soil_ksat, 0)
         water = where(water > soil_ksat, soil_ksat, water)
 
         # water balance through the stage 2 evaporation layer #
-        de = where(water >= pde + evap_2, self._zeros, pde + evap_2 - water)
+        de = where(water >= pde + evap_2, 0, pde + evap_2 - water)
 
-        water = where(water < pde + evap_2, self._zeros, water - (pde + evap_2))
+        water = where(water < pde + evap_2, 0, water - (pde + evap_2))
         # print 'water through  de  layer: {}'.format(mm_af(water))
 
         # water balance through the root zone layer #
         # print 'deep percolation total: {}'.format(mm_af(m['infil']))
-        dr = where(water >= pdr + transp, self._zeros, pdr + transp - water)
+        dr = where(water >= pdr + transp, 0, pdr + transp - water)
 
-        m['infil'] = where(water >= pdr + transp, water - pdr - transp, self._zeros)
+        m['infil'] = where(water >= pdr + transp, water - pdr - transp, 0)
         m['soil_storage'] = ((pdr - dr) + (pde - de) + (pdrew - drew))
         m['dr'] = dr
         m['de'] = de
@@ -457,14 +455,11 @@ class Processes(object):
         m['evap_2'] = evap_2
         m['soil_ksat'] = soil_ksat
 
-    def _do_vert_soil_water_balance(self, ro_local_reinfilt_frac=0.0, ceff=1.0):
+    def _do_vert_soil_water_balance(self, m, s, ro_local_reinfilt_frac=0.0, ceff=1.0):
         """ Calculate all soil water balance at each time step.
 
         :return: None
         """
-        m = self._master
-        s = self._static
-
         water = m['rain'] + m['melt']
 
         # m['dry_days'] = where(water < 0.1, m['dry_days'] + self._ones, self._ones)
@@ -513,8 +508,8 @@ class Processes(object):
         #                                                          mm_af(m['transp']))
 
         # water balance through skin layer #
-        drew = where(water >= pdrew + evap_1, self._zeros, pdrew + evap_1 - water)
-        water = where(water < pdrew + evap_1, self._zeros, water - pdrew - evap_1)
+        drew = where(water >= pdrew + evap_1, 0, pdrew + evap_1 - water)
+        water = where(water < pdrew + evap_1, 0, water - pdrew - evap_1)
         # print 'water through skin layer: {}'.format(mm_af(water))
 
         # m['ro'] = where(water > soil_ksat, water - soil_ksat, self._zeros)
@@ -528,17 +523,17 @@ class Processes(object):
 
         # water balance through the stage 2 evaporation layer #
         v = pde + evap_2
-        de = where(water >= v, self._zeros, v - water)
+        de = where(water >= v, 0, v - water)
 
-        water = where(water < v, self._zeros, water - v)
+        water = where(water < v, 0, water - v)
         # print 'water through  de  layer: {}'.format(mm_af(water))
 
         # water balance through the root zone layer #
         v = pdr + transp
-        m['infil'] = where(water >= v, water - pdr - transp, self._zeros)
+        m['infil'] = where(water >= v, water - pdr - transp, 0)
 
         # print 'deep percolation total: {}'.format(mm_af(m['infil']))
-        dr = where(water >= v, self._zeros, v - water)
+        dr = where(water >= v, 0, v - water)
 
         m['soil_storage'] = ((pdr - dr) + (pde - de) + (pdrew - drew))
 
@@ -653,15 +648,15 @@ class Processes(object):
         # print 'raw temp nan values = {}'.format(count_nonzero(isnan(m['temp'])))
 
         precip = get_prism(self._mask_path, prism, date, variable='precip')
-        m['precip'] = where(precip < self._zeros, self._zeros, precip)
+        m['precip'] = where(precip < 0, 0, precip)
         # print 'raw precip nan values = {}'.format(count_nonzero(isnan(m['precip'])))
         # print 'precip min {} precip max on day {} is {}'.format(m['precip'].min(), date, m['precip'].max())
         # print 'total precip et on {}: {:.2e} AF'.format(date, (m['precip'].sum() / 1000) * (250 ** 2) / 1233.48)
 
         etrs = get_penman(self._mask_path, penman, date, variable='etrs')
         # print 'raw etrs nan values = {}'.format(count_nonzero(isnan(m['etrs'])))
-        etrs = where(etrs < self._zeros, self._zeros, etrs)
-        m['etrs'] = where(isnan(etrs), self._zeros, etrs)
+        etrs = where(etrs < 0, 0, etrs)
+        m['etrs'] = where(isnan(etrs), 0, etrs)
         # print 'bounded etrs nan values = {}'.format(count_nonzero(isnan(m['etrs'])))
         # print 'total ref et on {}: {:.2e} AF'.format(date, (m['etrs'].sum() / 1000) * (250 ** 2) / 1233.48)
 
@@ -669,10 +664,7 @@ class Processes(object):
 
         m['pkcb'] = m['kcb']
 
-    def _do_parameter_adjustment(self, adjustment_array):
-
-        m = self._master
-        s = self._static
+    def _do_parameter_adjustment(self, m, s, adjustment_array):
 
         alpha, beta, gamma, delta, zeta, theta = adjustment_array
         if m['first_day']:
@@ -686,9 +678,7 @@ class Processes(object):
         m['kcb'] *= zeta
         m['soil_ksat'] *= theta
 
-    def _update_master_tracker(self, date):
-        m = self._master
-
+    def _update_master_tracker(self, m, date):
         def factory(k):
             v = m[k]
             try:
