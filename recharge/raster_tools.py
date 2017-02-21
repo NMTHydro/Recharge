c19a4b04ce2d65650f25680605b37e9f2ff39659
Jake Ross <jirhiker@gmail.com>
2017-02-21 16:05:35 -0700

    minor cleanup

--- recharge/raster_tools.py
+++ recharge/raster_tools.py
@@ -25,8 +25,7 @@ from datetime import datetime
 import os
 
 
-def convert_raster_to_array(input_raster_path, raster,
-                            band=1):
+def convert_raster_to_array(input_raster_path, raster, band=1):
     """
     Convert .tif raster into a numpy numerical array.
 
@@ -40,16 +39,18 @@ def convert_raster_to_array(input_raster_path, raster,
     return ras
 
 
-def get_raster_geo_attributes(statics_path):
+def get_raster_geo_attributes(root):
     """
     Creates a dict of geographic attributes from any of the pre-processed standardized rasters.
 
-    :param statics_path: Path to a folder with pre-processed standardized rasters.
+    :param root: Path to a folder with pre-processed standardized rasters.
     :return: dict of geographic attributes.
     """
-    statics = [filename for filename in os.listdir(statics_path) if filename.endswith('.tif')]
-    file_name = statics[0]
-    dataset = gdal.Open(os.path.join(statics_path, file_name))
+    # statics = [filename for filename in os.listdir(statics_path) if filename.endswith('.tif')]
+    # file_name = statics[0]
+    file_name = next((fn for fn in os.listdir(root) if fn.endswith('.tif')), None)
+    dataset = gdal.Open(os.path.join(root, file_name))
+
     band = dataset.GetRasterBand(1)
     raster_geo_dict = {'cols': dataset.RasterXSize, 'rows': dataset.RasterYSize, 'bands': dataset.RasterCount,
                        'data_type': band.DataType, 'projection': dataset.GetProjection(),
