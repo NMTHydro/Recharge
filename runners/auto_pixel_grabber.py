4005577d77969f9ab792f42405ea3ab695f80eba
Gabe <gbrlparrish@gmail.com>
2017-07-02 16:26:27 -0600

    final touches on auto_pixel grabber. For now.

--- runners/auto_pixel_grabber.py
+++ runners/auto_pixel_grabber.py
@@ -37,6 +37,7 @@ def pathlist_make(pixel_dir):
     gets the directory where the pixel rasters are and returns a list of the full file paths for each
     raster in the directory
     """
+
     # grab the files in the directory
     for files in os.walk(pixel_dir, topdown=True):
         for name in files:
@@ -67,7 +68,7 @@ def dict_maker(directory_list):
         for item in directory_list:
             item_list = item.split('.')
             sel = item_list[0]
-            label = sel[-19:]
+            label = sel[-20:]
 
             location_dict[label] = item
 
@@ -204,8 +205,6 @@ def formatter(obs, model):
     return final_mod, final_obs
 
 
-
-
 def frameit(mod_dict, obs_dict):
     """
     takes list of
@@ -229,9 +228,11 @@ def frameit(mod_dict, obs_dict):
 
         df_mod = pd.DataFrame(tuple_list_sort)
 
-        print 'df', df_mod
+        #print 'df', df_mod
 
-        mod[pixel] = df_mod
+        rev_dfmod = df_mod.iloc[::-1]
+
+        mod[pixel] = rev_dfmod
 
     for pixel, tuple_list in obs_dict.items():
 
@@ -242,14 +243,17 @@ def frameit(mod_dict, obs_dict):
 
         df_obs = pd.DataFrame(tuple_list_sort)
 
-        print 'df obs', df_obs
+        #print 'df obs', df_obs
 
-        obs[pixel] = df_obs
+        #=== HERE I reverse the order bc of how my plotting program is set up.
+        rev_dfobs = df_obs.iloc[::-1]
+
+        obs[pixel] = rev_dfobs
 
         #obs_list.append(obs)
 
     #print 'obs list', obs_list
-    print 'obs', obs
+    #print 'obs', obs
 
     for pixel, dat_f in mod.items():
 
@@ -257,19 +261,43 @@ def frameit(mod_dict, obs_dict):
 
         together_frame[pixel] = frame
 
-    print together_frame
+    print 'together frame', together_frame
     return together_frame
+
+def xml_write(frame_dict):
+    """
+    Takes a dataframe and writes to excel. This is the main output of this script.
+    The output xml is used by a plotting script.
+    """
+
+    for pixel in frame_dict.keys():
+
+        #print 'pixel contents -----', frame_dict[pixel]
+
+        pd.DataFrame.to_csv(frame_dict[pixel], '/Users/Gabe/Desktop/RZSM_analysis/pixels_6/{}.csv'.format(pixel), index=False, header=None)
 
 
 def run():
+    """
+    takes a pixel mask, or a list of pixel masks
+
+    takes model output rasters
+
+    grabs the pixels corresponding to the mask that are in the rasters
+
+    puts the pixels into a dataframe and or nested dict.
+
+    ***** Changes should be made to soil_moisture_mapper also *****
+
+    """
 
     print 'my hairy butt'
 
     # get pixel shapefile
-    pixel_file = "/Users/Gabe/Desktop/pixel_masks/pixel_point_0.shp"
+    pixel_file = "/Users/Gabe/Desktop/pixel_masks/pixel_shapefile.shp"
 
     #=====Get observations from somewhere.=======
-    obs_dir = "/Users/Gabe/Desktop/rzsm_observations"
+    obs_dir = "/Users/Gabe/Desktop/rzsm_observations_updated"
     obs_directory_list = pathlist_make(obs_dir)
     obs_dir_dict = dict_maker(obs_directory_list)
 
@@ -285,253 +313,12 @@ def run():
 
     frame_dict = frameit(final_mod, final_obs)
 
+
+
     # TODO - func to write frames to xml and save so pixel_plotter can work w em.
+
+    xml_write(frame_dict)
 
 
 if __name__ == "__main__":
     run()
-
-
-#==========================================
-"""
-takes a pixel mask, or a list of pixel masks
-
-takes model output rasters
-
-grabs the pixels corresponding to the mask that are in the rasters
-
-puts the pixels into a dataframe and or nested dict.
-
-***** Changes should be made to soil_moisture_mapper also *****
-
-"""
-
-
-
-#++++++=========== Saved Changes June 24, 2017 bc of switch to GDAL based Design
-
-# def pathlist_make(pixel_dir):
-#     """
-#
-#     :param pixel_dir:
-#     :return:
-#
-#     gets the directory where the pixel rasters are and returns a list of the full file paths for each
-#     raster in the directory
-#     """
-#     # grab the files in the directory
-#     for files in os.walk(pixel_dir, topdown=True):
-#         for name in files:
-#             file_list = name
-#
-#     # remove the non-tiff files
-#     for file in file_list:
-#         if not file.endswith('.tif'):
-#             file_list.remove(file)
-#     print 'file list', file_list
-#     path_list = [os.path.join(pixel_dir, item) for item in file_list]
-#     print 'path list', path_list
-#
-#     return path_list
-#
-# def location_grab(directory_list):
-#     """
-#     uses the list of file directories constructed by pathlist_make(), turns the .tif into an array.
-#     Gets the location(s?) of the non-zero value of the array.
-#     Returns a dict of array locations.
-#     In the case of the obs and model results, merely returns a dict of file name and path for later use.
-#
-#     :return:
-#     """
-#     location_dict = {}
-#
-#     # gonna look like {pixel_00: location, pixel_01:location}
-#     if 'pixel_rast' in directory_list[0]:
-#         for item in directory_list:
-#             array = convert_raster_to_array(item)
-#             item_list = item.split('.')
-#             print 'item_list', item_list
-#             sel = item_list[0]
-#             label = sel[-13:]
-#             # label is the title for the dict
-#
-#             # print 'array', array
-#             # print'shape of the array', array.shape
-#
-#             location = np.where(array == 1)
-#             print 'location', location
-#             #add the label and location to the dict
-#             location_dict[label] = location
-#
-#         print 'location dict', location_dict
-#         return location_dict
-#
-#     elif 'rzsm_u_' in directory_list[0]:
-#         for item in directory_list:
-#             item_list = item.split('.')
-#             sel = item_list[0]
-#             label = sel[-19:]
-#
-#             location_dict[label] = item
-#
-#         #print 'rzsm obs location dict', location_dict
-#         return location_dict
-#
-#     elif 'rzsm_' in directory_list[0]:
-#         for item in directory_list:
-#             item_list = item.split('.')
-#             sel = item_list[0]
-#             label = sel[-18:]
-#
-#             location_dict[label] = item
-#
-#         #print 'rzsm simulation location dict', location_dict
-#         return location_dict
-#
-# def array_converter(dict_of_paths):
-#     """
-#     :param dict_of_paths:
-#     :return:
-#
-#     Takes the paths for files that need to be converted into arrays.
-#     The arrays are stored in a dict with the same keys as the dict of paths.
-#     """
-#     array_dict = {}
-#     for name, path in dict_of_paths.items():
-#         array = convert_raster_to_array(path)
-#         array_dict[name] = array
-#
-#     print 'array dict! ', array_dict
-#     return array_dict
-#
-# def value_packer(pixel, obs, model):
-#     """
-#     Gets the model array and
-#     :param pixel:
-#     :param obs:
-#     :param model:
-#     :return:
-#     """
-#     # master = {}
-#     # for each pixel location
-#
-#         # for each obs value
-#             # get value at location (HOW?)
-#             # store value master[obskey] = value
-#
-#         # for each model value
-#             # get value at location
-#             # store value master[modelskey] = value
-#
-#     # return master
-#
-#     master = {}
-#
-#     for pixel, location in pixel.items():
-#
-#         # for observ, obs_arr in obs.items():
-#         #
-#         #     print obs_arr.shape
-#         #
-#         #     master[pixel][observ] = obs_arr[location]
-#
-#         for mod, mod_arr in model.items():
-#
-#             master[mod] = mod_arr[location]
-#
-#     return master
-#
-#
-# def run():
-#
-#     pixel_dir = "/Users/Gabe/Desktop/pixel_masks"
-#
-#     #=====Get observations from somewhere.=======
-#     obs_dir = "/Users/Gabe/Desktop/rzsm_observations"
-#     obs_directory_list = pathlist_make(obs_dir)
-#     obs_dir_dict = location_grab(obs_directory_list)
-#
-#     #=====Get modeled results from somewhere.====
-#     model_dir = "/Users/Gabe/Desktop/modeled_rzsm"
-#     model_dir_list = pathlist_make(model_dir)
-#     model_dir_dict = location_grab(model_dir_list)
-#
-#     #===Get the obs and modeled results stored in a dict of arrays.====
-#     #==================================================================
-#     #==== Obs Arrays =======
-#     obs_arr_dict = array_converter(obs_dir_dict)
-#     #==== Modeled Arrays ===
-#     model_arr_dict = array_converter(model_dir_dict)
-#
-#     #===== Make a location dict for the array locations of data you need
-#     pixel_directory_list = pathlist_make(pixel_dir)
-#     location_dict = location_grab(pixel_directory_list)
-#
-#     #use location dict to grab the right values from the Observations and Model Results
-#     #store the values you get in a good format in a pixel by pixel dataframe.
-#     master_dict = value_packer(location_dict, obs_arr_dict, model_arr_dict)
-#     # TODO fix master dict for
-#
-#     print 'master dictionary', master_dict
-#
-#
-#
-#
-# if __name__ == "__main__":
-#     run()
-#
-#     #one_to_one_plotter()
-
-
-#======= Save of 'formatter()' func June 25, 2017 =======
-# def formatter(obs, model):
-#
-#     print 'len mod', len(model['rzsm_13_06_2004_000'])
-#     iterate = len(model['rzsm_13_06_2004_000'])
-#     obs_pixel_dict = {}
-#     mod_pixel_dict = {}
-#     obs_dict_list = []
-#     mod_dict_list = []
-#     count_1 = 0
-#     count_2 = 0
-#     key_dict = {}
-#     a_dict = {}
-#
-#     # for i in range(iterate):
-#     #     key_dict = {}
-#     #     a_dict = {}
-#
-#     for key, item in obs.items():
-#
-#         print 'key', key
-#         print 'item', item
-#
-#         #obs_pixel_dict = {'pixel_{}'.format(count_1):{key:item}}
-#         key_dict[key] = item
-#         ins_obs = key_dict
-#
-#         print 'ins obs', ins_obs
-#
-#         obs_pixel_dict['pixel_{}'.format(count_1)] = ins_obs
-#
-#         obs_dict_list.append(obs_pixel_dict)
-#
-#     count_1 += 1
-#     # TODO - ^^^ Fix below to be like above ^^^^
-#     # TODO - Need {pixel_0:{rzsm:{0: value, 1: value, 2: value} - You're going about it wrong.
-#     # TODO - idea -> Instead of fucking around with dictionaries, go to DF and manipulate the DF.
-#     for a, b in model.items():
-#
-#         #model_pixel_dict = {'pixel_{}'.format(count_2):{a:b}}
-#
-#         ins_mod = a_dict[key] = b
-#
-#         mod_pixel_dict['pixel_{}'.format(count_1)] = ins_mod
-#
-#         mod_dict_list.append(mod_pixel_dict)
-#
-#     count_2 += 1
-#
-#     print 'obs dict list', obs_dict_list
-#
-#     print 'mod_dict_list', mod_dict_list
